e4edd1016dfab967296d5d25a96514a8ec892032
Thomas van Dam <t.thomas.van.dam@gmail.com>
2015-03-18 14:11:23 +0100

    Result processing for visualisation.

---
+++ aggregateLanguages.py
@@ -0,0 +1,30 @@
+import json
+
+totalFile = open("totalsPerCity.json")
+totalData = json.load(totalFile)
+totalFile.close()
+
+totalLanguages = {}
+
+for city in totalData:
+  filePath = "results/" + city + "/" + city + "Languages.json"
+  langFile = open(filePath)
+  langData = json.load(langFile)
+  langFile.close()
+
+  for lang in langData:
+    if lang in totalLanguages:
+      totalLanguages[lang]["users"] += langData[lang]["users"]
+      totalLanguages[lang]["commits"] += langData[lang]["commits"]
+    else:
+      totalLanguages[lang] = {}
+      totalLanguages[lang]["users"] = langData[lang]["users"]
+      totalLanguages[lang]["commits"] = langData[lang]["commits"]
+
+for lang in totalLanguages:
+  totalLanguages[lang]["commitsPerUser"] = round(float(totalLanguages[lang]["commits"]) / round(totalLanguages[lang]["users"]), 2)
+
+outLanguages = "totalsPerLanguage.json"
+with open(outLanguages, "w") as outfile:
+  json.dump(totalLanguages, outfile)
+  print "Finished languages."
---
+++ tod3.py
@@ -0,0 +1,72 @@
+import json
+
+finalData = {}
+
+finalData["nodes"] = []
+finalData["links"] = []
+
+IDs = {}
+
+langFile = open("totalsPerLanguage.json")
+langData = json.load(langFile)
+langFile.close()
+
+cityFile = open("totalsPerCity.json")
+cityData = json.load(cityFile)
+cityFile.close()
+
+newID = 0
+
+# Create nodes
+for city in cityData:
+  tempCity = {}
+  tempCity["ID"] = newID
+  tempCity["name"] = city
+  tempCity["type"] = "city"
+  tempCity["commits"] = cityData[city]["commits"]
+  tempCity["users"] = cityData[city]["users"]
+  tempCity["commitsPerUser"] = cityData[city]["commitsPerUser"]
+
+  IDs[city] = newID
+  newID += 1
+
+  finalData["nodes"].append(tempCity)
+
+for lang in langData:
+  tempLang = {}
+
+  tempLang["ID"] = newID
+  tempLang["name"] = lang
+  tempLang["type"] = "lang"
+  tempLang["commits"] = langData[lang]["commits"]
+  tempLang["users"] = langData[lang]["users"]
+  tempLang["commitsPerUser"] = langData[lang]["commitsPerUser"]
+
+  IDs[lang] = newID
+  newID += 1
+
+  finalData["nodes"].append(tempLang)
+
+# Create links
+for city in cityData:
+  cityLangFilePath = "results/" + city + "/" + city + "Languages.json"
+  cityLangFile = open(cityLangFilePath)
+  cityLangData = json.load(cityLangFile)
+  cityLangFile.close()
+
+  for lang in cityLangData:
+    tempLink = {}
+
+    tempLink["source"] = IDs[city]
+    tempLink["target"] = IDs[lang]
+    tempLink["commits"] = cityLangData[lang]["commits"]
+    tempLink["users"] = cityLangData[lang]["users"]
+    tempLink["commitsPerUser"] = cityLangData[lang]["commitsPerUser"]
+
+    finalData["links"].append(tempLink)
+
+outFinal = "data.json"
+with open(outFinal, "w") as outfile:
+  json.dump(finalData, outfile)
+  print "Finished all the things."
+
